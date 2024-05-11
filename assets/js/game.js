2cfac9c4d385fcb44603887c1a368610b87d624d
Walter Rodríguez C <carowa-15@hotmail.com>
2024-05-11 06:55:21 -0500

    Se optimizan procesos y protege el código JS usando patrón Modelo.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,107 +1,141 @@
 /**
- * 2C = Two of Clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
+ * Patrón de diseño Módulo de JS
+ * 
+ * Función anónima autoinvocada
  */
-let deck = [];
-const tipos = ['C', 'D', 'H', 'S'];
-const espaciales = ['A', 'J', 'Q', 'K'];
+const modBlackJack = (() => {
+    // Restricciones: Evaluar código de forma estricta
+    'use strict'
+    
+    /**
+     * 2C = Two of Clubs
+     * 2D = Two of Diamonds
+     * 2H = Two of Hearts
+     * 2S = Two of Spades
+     */
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'],
+          espaciales = ['A', 'J', 'Q', 'K'];
 
-// Puntos
-let puntosJugador = 0,
-    puntosMaquina = 0;
+    // Puntos
+    let puntosJugadores = [];
 
-// Referencias del HTML
-const btnNuevo = document.querySelector('#btnNuevo');
-const btnPedir = document.querySelector('#btnPedir');
-const btnDetener = document.querySelector('#btnDetener');
-const cartasJugador = document.querySelector('#cartasJugador');
-const cartasMaquina = document.querySelector('#cartasMaquina');
-const puntosHTML = document.querySelectorAll('small');
+    // Referencias del HTML
+    const btnNuevo  = document.querySelector('#btnNuevo'),
+          btnPedir  = document.querySelector('#btnPedir'),
+          btnDetener = document.querySelector('#btnDetener');
 
-/**
- * Función para crear una nueva baraja
- */
-const crearDeck = () => {
-    // Crear baraja de cartas
-    for (let i = 2; i <= 10; i++) {
-        for (let tipo of tipos) {
-            deck.push( i + tipo );
-        }        
-    }
+    const cartasJugador = document.querySelectorAll('.cartas'),
+          puntosHTML = document.querySelectorAll('small');
+    
+    // Inicializa el juego
+    const iniciarJuego = ( numJugadores = 2 ) => {
+        // Crear nueva bajara de cartas
+        deck = crearDeck();
 
-    // Agregar cartas especiales a la baraja
-    for (let tipo of tipos) {
-        for (let esp of espaciales) {
-            deck.push( esp + tipo );
+        // Resetear puntos jugadores
+        puntosJugadores = [];
+
+        // 
+        for (let i = 0; i < numJugadores; i++) {
+            puntosJugadores.push(0);            
         }
+        
+        // Limpiar puntos jugadores en la vista
+        puntosHTML.forEach( elem => elem.innerText = 0 );
+
+        // Limpiar cartas vista HTML
+        cartasJugador.forEach( carta => carta.innerHTML = '' );
+
+        // Habilitar botón Pedir carta
+        btnPedir.disabled = false;
+
+        // Habilitar botón Detener
+        btnDetener.disabled = false;
     }
 
-    // Barajar cartas usando un algoritmo
-    // algoritmo de Fisher-Yates que es el que se usa típicamente para barajar en los juegos de azar (por lo que pude investigar).
-    // const barajar=(arreglo)=>{     let i = arreglo.length;     while(--i>0){         let randIndex = Math.floor(Math.random() * (i + 1));         [arreglo[randIndex], arreglo[i]] = [arreglo[i], arreglo[randIndex]];     }     return arreglo; }
+    /**
+     * Función para crear una nueva baraja
+     */
+    const crearDeck = () => {
+        // Limpiar deck
+        deck = [];
 
-    // Barajar cartas usando una librería
-    deck = _.shuffle( deck );
-    
-    return deck;
-}
+        // Crear baraja de cartas
+        for (let i = 2; i <= 10; i++) {
+            for (let tipo of tipos) {
+                deck.push( i + tipo );
+            }        
+        }
 
-crearDeck();
+        // Agregar cartas especiales a la baraja
+        for (let tipo of tipos) {
+            for (let esp of espaciales) {
+                deck.push( esp + tipo );
+            }
+        }
 
-/**
- * Función para tomar carta
- */
-const pedirCarta = () => {
-    if ( deck.length === 0 ) {
-        throw 'No hay carta en el deck.';
+        // Barajar cartas usando un algoritmo
+        // algoritmo de Fisher-Yates que es el que se usa típicamente para barajar en los juegos de azar (por lo que pude investigar).
+        // const barajar=(arreglo)=>{     let i = arreglo.length;     while(--i>0){         let randIndex = Math.floor(Math.random() * (i + 1));         [arreglo[randIndex], arreglo[i]] = [arreglo[i], arreglo[randIndex]];     }     return arreglo; }
+
+        // Barajar cartas usando una librería y retornar resultado
+        return _.shuffle( deck );
     }
 
-    // Remover última carta de la baraja
-    const carta = deck.pop();
+    /**
+     * Función para tomar carta
+     */
+    const pedirCarta = () => {
+        if ( deck.length === 0 ) {
+            throw 'No hay carta en el deck.';
+        }
+        
+        // Remover última carta de la baraja y devoler
+        return deck.pop();
+    }
 
-    // Devoler la última carta removida de la baraja
-    return carta;
-}
+    /**
+     * Obtener valor carta
+     */
+    const valorCarta = ( carta ) => {
+        // Obtener string ignorando la última letra
+        const valor = carta.substring(0, carta.length - 1);
 
-/**
- * Obtener valor carta
- */
-const valorCarta = ( carta ) => {
-    // Obtener string ignorando la última letra
-    const valor = carta.substring(0, carta.length - 1);
-
-    return ( isNaN( valor ) ) ? 
-            ( valor === 'A' ) ? 11 : 10
-            : valor * 1;
+        return ( isNaN( valor ) ) ? 
+                ( valor === 'A' ) ? 11 : 10
+                : valor * 1;
+        
+        // let puntos = 0;
+        // // Si el valor de la letra es un número...
+        // if ( isNaN( valor ) ) {
+        //     puntos = ( valor === 'A' ) ? 11 : 10;
+        // } else {
+        //     puntos = valor * 1;
+        // }
+    }
     
-    // let puntos = 0;
-    // // Si el valor de la letra es un número...
-    // if ( isNaN( valor ) ) {
-    //     puntos = ( valor === 'A' ) ? 11 : 10;
-    // } else {
-    //     puntos = valor * 1;
-    // }
-}
-
-/**
- * Lógica turno máquina
- */
-const turnoMaquina = ( puntosMinimos ) => {
-    do {
-        // Obtener carta
-        const carta = pedirCarta();
-
+    /**
+     * Acumula puntos del jugador actual
+     * 
+     * 
+     * @param {*} turno | La última pocisión del arreglo (0) será de la máquina y los primeros de los jugadores.
+     */
+    const acumularPuntos = ( carta, turno ) => {
         // Obtener valor carta y calcular puntaje actual de la máquina
-        puntosMaquina = puntosMaquina + valorCarta( carta );
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta( carta );
 
         // Actualizar puntaje en el la vista HTML
-        puntosHTML[1].innerText = puntosMaquina;
+        puntosHTML[turno].innerText = puntosJugadores[turno];
 
+        return puntosJugadores[turno];
+    }
+
+    // Crear carta
+    const crearCarta = ( carta, turno ) => {
         // Crear elemento img para la carta
         const imgCarta = document.createElement('img');
-        
+            
         // Actualizar ruta imagen
         imgCarta.src = `assets/cartas/${ carta }.png`;
 
@@ -109,123 +143,123 @@ const turnoMaquina = ( puntosMinimos ) => {
         imgCarta.classList.add('carta');
 
         // Agregar elemento (imagen carta) al DOM
-        cartasMaquina.append( imgCarta );
-
-        // Romper ciclo si la máquina ya ganó, es decir tiene 21 puntos
-        if ( puntosJugador > 21 ) {
-            break;
-        }
-    } while ( ( puntosMaquina < puntosMinimos ) && ( puntosMinimos <= 21 ) );
-
-    // Validar ganador y mostrar mensaje
-    setTimeout(() => {
-        if ( puntosMaquina === puntosMinimos ) {
-            alert('Empate');
-        } else if ( puntosMinimos > 21 ) {
-            alert('Máquina ganó');
-        } else if ( puntosMaquina > 21 ) {
-            alert('Ganaste.');
-        } else {
-            alert('Máquina ganó');
-        }
-    }, 10);
-}
-
-/** 
- * Botón Pedir carta
- * 
- * Escucha el evento Click en el botón Pedir carta.
- * 
-*/
-btnPedir.addEventListener('click', () => {
-    // Obtener carta
-    const carta = pedirCarta();
-
-    // Obtener valor carta y calcular puntaje actual del jugador
-    puntosJugador = puntosJugador + valorCarta( carta );
-
-    // Actualizar puntaje en el la vista HTML
-    puntosHTML[0].innerText = puntosJugador;
-
-    // Crear elemento img para la carta
-    const imgCarta = document.createElement('img');
-    
-    // Actualizar ruta imagen
-    imgCarta.src = `assets/cartas/${ carta }.png`;
-
-    // Agregar clase CSS al elemento img para la carta
-    imgCarta.classList.add('carta');
-
-    // Agregar elemento (imagen carta) al DOM
-    cartasJugador.append( imgCarta );
-
-
-    // Validar si el jugador perdió
-    if ( puntosJugador > 21 ) {
-        // Deshabilitar botón Pedir carta
-        btnPedir.disabled = true;
-        
-        // Deshabilitar botón Detener
-        btnDetener.disabled = true;
-        
-        // Ejecutar turno máquina
-        turnoMaquina( puntosJugador );        
-    } else if ( puntosJugador === 21 ) {
-        // Mostrar mensaje
-        alert('21, genial');
-
-        // Deshabilitar botón Pedir carta
-        btnPedir.disabled = true;
-
-        // Deshabilitar botón Detener
-        btnDetener.disabled = true;
-
-        // Ejecutar turno máquina
-        turnoMaquina( puntosJugador );
+        cartasJugador[turno].append( imgCarta );
     }
-});
 
-/** 
- * Botón Detener
- * 
- * Escucha el evento Click en el botón Detener.
- * 
-*/
-btnDetener.addEventListener('click', () => {   
-    // Deshabilitar botón Pedir carta
-    btnPedir.disabled = true;
+    /**
+     * Determinar ganador
+     */
+    const determinarGanador = () => {
+        // Destructurar el arreglo
+        const [ puntosMinimos, puntosMaquina ] = puntosJugadores;
 
-    // Deshabilitar botón Detener
-    btnDetener.disabled = true;
+        // Validar ganador y mostrar mensaje
+        setTimeout(() => {
+            if ( puntosMaquina === puntosMinimos ) {
+                alert('Empate');
+            } else if ( puntosMinimos > 21 ) {
+                alert('Máquina ganó');
+            } else if ( puntosMaquina > 21 ) {
+                alert('Ganaste.');
+            } else {
+                alert('Máquina ganó');
+            }
+        }, 10);
+    }
 
-    // Ejecutar turno máquina
-    turnoMaquina( puntosJugador );
-});
+    /**
+     * Lógica turno máquina
+     */
+    const turnoMaquina = ( puntosMinimos ) => {
+        let puntosMaquina = 0;
 
-/**
- * Nuevo juego
- * 
- * Resetea todo para empezar un nuevo juego.
- */
-btnNuevo.addEventListener('click', () => {
-    // Limpiar baraja
-    deck = [];
+        do {
+            // Obtener carta
+            const carta = pedirCarta();
 
-    // Crear nueva bajara de cartas
-    deck = crearDeck();
+            // Calcular puntos máquina
+            puntosMaquina = acumularPuntos( carta, puntosJugadores.length - 1 );
 
-    puntosJugador = 0;
-    puntosMaquina = 0;
+            // Crear carta
+            crearCarta( carta, puntosJugadores.length - 1 );
 
-    puntosHTML[0].innerText = 0;
-    puntosHTML[1].innerText = 0;
+        } while ( ( puntosMaquina < puntosMinimos ) && ( puntosMinimos <= 21 ) );    
+        
+        // Validar ganador
+        determinarGanador();
+    }
 
-    cartasJugador.innerHTML = '';
-    cartasMaquina.innerHTML = '';
+    /** 
+     * Botón Pedir carta
+     * 
+     * Escucha el evento Click en el botón Pedir carta.
+     * 
+    */
+    btnPedir.addEventListener('click', () => {
+        // Obtener carta
+        const carta = pedirCarta();
 
-    // Habilitar botón Pedir carta
-    btnPedir.disabled = false;
+        // Calcular puntos Jugador
+        const puntosJugador = acumularPuntos( carta, 0 );
 
-    // Habilitar botón Detener
-    btnDetener.disabled = false;
-});
+        // Crear carta
+        crearCarta( carta, 0);
+
+        // Validar si el jugador perdió
+        if ( puntosJugador > 21 ) {
+            // Deshabilitar botón Pedir carta
+            btnPedir.disabled = true;
+            
+            // Deshabilitar botón Detener
+            btnDetener.disabled = true;
+            
+            // Ejecutar turno máquina
+            turnoMaquina( puntosJugador );        
+        } else if ( puntosJugador === 21 ) {
+            // Mostrar mensaje
+            alert('21, genial');
+
+            // Deshabilitar botón Pedir carta
+            btnPedir.disabled = true;
+
+            // Deshabilitar botón Detener
+            btnDetener.disabled = true;
+
+            // Ejecutar turno máquina
+            turnoMaquina( puntosJugador );
+        }
+    });
+
+    /** 
+     * Botón Detener
+     * 
+     * Escucha el evento Click en el botón Detener.
+     * 
+    */
+    btnDetener.addEventListener('click', () => {   
+        // Deshabilitar botón Pedir carta
+        btnPedir.disabled = true;
+
+        // Deshabilitar botón Detener
+        btnDetener.disabled = true;
+
+        // Ejecutar turno máquina
+        turnoMaquina( puntosJugadores[0] );
+    });
+
+    /**
+     * Nuevo juego
+     * 
+     * Resetea todo para empezar un nuevo juego.
+     */
+    btnNuevo.addEventListener('click', () => {
+        iniciarJuego();    
+    });
+
+    // Lo que aquí retorne es lo único que va hacer público/visible fuera de este módulo lo cual solo se podrá acceder a él mediante el módulo.
+    return {
+        newGame: iniciarJuego
+    };
+
+})();
+
